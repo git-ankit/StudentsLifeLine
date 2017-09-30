b5078b0c961d49d87896a73c5874b1a41e9335d7
sagarv1997 <sagarv1997@gmail.com>
2017-09-30 15:50:58 +0530

    css done

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -9,22 +9,21 @@
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-
 <header>
 	<nav>
 		<div class="main-wrapper">
 			<ul>
 				<li><a href="index.php">Home</a></li>
 			</ul>
-			<div class="nav-login">
+			<div class="nav-login"">
 				<form action="includes/login.inc.php" method="POST">
 					<input type="text" name="uid" placeholder="Username/E-mail">
 					<input type="password" name="pwd" placeholder="Password">
 					<button type="submit" name="submit">Login</button>
 					<button><a href="signup.php">Sign up</a></button>
 				</form>
-				
 			</div>
 		</div>
 	</nav>
 </header>
+</body>
