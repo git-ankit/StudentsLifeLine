8deb5f32e795e82b68861d0032e45a50e5d81590
sagarv1997 <sagarv1997@gmail.com>
2017-09-30 11:31:25 +0530

    Signup Validation complete

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -3,7 +3,6 @@
 if (isset($_POST['submit'])) {
 
 	include_once 'dbh.inc.php';
-	echo "string";
 
 	$first = mysqli_real_escape_string($conn, $_POST['first']);
 	$last = mysqli_real_escape_string($conn, $_POST['last']);
@@ -11,18 +10,56 @@ if (isset($_POST['submit'])) {
 	$uid = mysqli_real_escape_string($conn, $_POST['uid']);
 	$pwd = mysqli_real_escape_string($conn, $_POST['pwd']);
 
-}
+	// Error Handlers
+	// Check for empty fields
+
+	if (empty($first) || (empty($last) || empty($email) || empty($uid) || empty($pwd)) {
+		header ("Location: ../signup.php?signup=empty");
+		exit();
+	} else {
+		// Check if input characters are valid
+		if (!preg_match("/^[a-zA-Z]*$", $first) || (!preg_match("/^[a-zA-Z]*$", $last)) {
+			header ("Location: ../signup.php?signup=invalid");
+			exit();
+		} else {
+			// Check if email is valid 
+			if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+				header ("Location: ../signup.php?signup=email");
+				exit();
+			} else {
+				// Check for username available or not
+				$sql = "SELECT * FROM users WHERE user_uid='$uid'";
+				$result = mysqli_query ($conn, $sql);
+				$resulCheck = mysqli_num_rows($result);
+
+				if ($resulCheck > 0) {
+					header ("Location: ../signup.php?signup=usertaken");
+					exit();
+				} else {
+					// Hashing the password
+					$hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+					// Insert the user into the database
+					$sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd) VALUES ('$first', '$last', '$email', '$uid', '$hashedPwd';";
+		 			mysqli_query($conn, $sql);
+		 			header("Location: ../signup.php?signup=success");
+				}
+			}
+		}
+	}
+
 else {
 		header ("Location: ../signup.php");
 	exit();
 }
 
-		 	// Hashing the password
-		 	$hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
-		 	// Insert
-		 	$sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd) VALUES ('$first', '$last', '$email', '$uid', '$hashedPwd';";
-		 	mysqli_query($conn, $sql);
-		 	header("Location: ../signup.php?signup=success");
-		 
+
+
+
+
+
+
+
+
+
 
 
