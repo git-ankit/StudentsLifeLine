8061a0969586ae613f98466168c911701e2625c1
sagarv1997 <sagarv1997@gmail.com>
2017-09-30 15:16:25 +0530

    Signup button correction

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -21,8 +21,9 @@
 					<input type="text" name="uid" placeholder="Username/E-mail">
 					<input type="password" name="pwd" placeholder="Password">
 					<button type="submit" name="submit">Login</button>
+					<button><a href="signup.php">Sign up</a></button>
 				</form>
-				<a href="signup.php">Sign up</a>
+				
 			</div>
 		</div>
 	</nav>
