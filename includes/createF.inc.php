54d4e2146be1d419a50f73bef4b2c9092ccae017
Ankit Tiwari <imankitiwari@gmail.com>
2017-10-06 00:22:58 +0530

    added session handler and a page which displayes all forums(css remaining). Fixed Login and create forum

--- includes/createF.inc.php
+++ includes/createF.inc.php
@@ -3,64 +3,47 @@
 if (isset($_POST['submit'])) {
 
 	include_once 'dbh.inc.php';
-
+	include_once 'session_manager.inc.php';
 	$name = mysqli_real_escape_string($conn, $_POST['name']);
 	$description = mysqli_real_escape_string($conn, $_POST['description']);
+	$created_by = mysqli_real_escape_string($conn, $_SESSION['u_id']);
 	
 	// Error Handlers
 	// Check for empty fields
 
 	if (empty($name) || empty($description)) {
-		$url = "Location: ../forum/createF.inc.php?signup=empty";
+		$url = "Location: ../forum/create.php?create=empty";
 		header ($url);
 		exit();	
 	} else {
 		// Check if input characters are valid
 		if ((!preg_match("/^[a-zA-Z]*$/", $first)) || (!preg_match("/^[a-zA-Z]*$/", $last))) {
-			header ("Location: ../signup.php?signup=invalid");
+			header ("Location: ../forum/create.php?create=invalid");
 			exit();
 		} else {
-			// Check if email is valid 
-			if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-				header ("Location: ../signup.php?signup=email");
+			// Check for forum name is available or not
+			$sql = "SELECT * FROM `forum` WHERE `name` = '$name'";
+			$result = mysqli_query ($conn, $sql);
+			$resulCheck = mysqli_num_rows($result);
+
+			if ($resulCheck > 0) {
+				header ("Location: ../forum/create.php?create=forum_name_taken");
 				exit();
 			} else {
-				// Check for username available or not
-				$sql = "SELECT * FROM users WHERE user_uid='$uid'";
-				$result = mysqli_query ($conn, $sql);
-				$resulCheck = mysqli_num_rows($result);
-
-				if ($resulCheck > 0) {
-					header ("Location: ../signup.php?signup=usertaken");
-					exit();
-				} else {
-					// Hashing the password
-					$hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
-					// Insert the user into the database
+				
+				// Insert the user into the database
 					
-					$sql = "INSERT INTO `users` (`user_first`, `user_last`, `user_email`, `user_uid`, `user_pwd`) VALUES ('$first', '$last', '$email', '$uid', '$hashedPwd')";
-		 			if(mysqli_query($conn, $sql) == true) {
-		 				header("Location: ../signup.php?signup=success");	
-		 			}
-		 			
-				}
+				$sql = "INSERT INTO `forum` (`name`, `description`, `created_by`) VALUES ('$name', '$description', '$created_by')";
+				if(mysqli_query($conn, $sql) == true) {
+					header("Location: ../forum/create.php?create=success");	
+		 		}
+		 		
 			}
+			
 		}
 	}
 
 } else {
-		header ("Location: ../signup.php");
+		header ("Location: ../forum/create.php");
 		exit();
 }
-
-
-
-
-
-
-
-
-
-
-
-
--- includes/login.inc.php
+++ includes/login.inc.php
@@ -15,8 +15,8 @@ if (isset($_POST['submit'])) {
 		header("Location ../index.php?login=empty");
 		exit();
 	} else {
-		$sql = "SELECT * FROM users WHERE user_uid='$uid'";
-		$result = mysqli_query ($conn, $sql);
+		$sql = "SELECT * FROM `users` WHERE `user_uid` = '$uid'";
+		$result = mysqli_query($conn, $sql);
 		$resultCheck = mysqli_num_rows($result);
 		if ($resultCheck < 1) {
 			header("Location ../index.php?login=error");
@@ -26,15 +26,17 @@ if (isset($_POST['submit'])) {
 				// De-hashing the password
 				$hashedPwdCheck = password_verify($pwd, $row['user_pwd']);
 				if ($hashedPwdCheck == false) {
-					header("Location ../index.php?login=error");
+					header("Location: ../index.php?login=error");
 					exit();
 				} elseif ($hashedPwdCheck == true) {
 					// Finally Matched :P
-					$_SESSION['u_id'] = $row['user_id'];
+					echo "hi";
+					$_SESSION['u_id'] = $row['id'];
 					$_SESSION['u_first'] = $row['user_first'];
 					$_SESSION['u_last'] = $row['user_last'];
 					$_SESSION['u_email'] = $row['user_email'];
 					$_SESSION['u_uid'] = $row['user_uid'];
+					$_SESSION['logged_in'] = true;
 					header("Location: ../index.php?login=sucess");
 					exit();
 
