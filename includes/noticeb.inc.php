0dc7b84a3f7c2893294839880224f8b4c0e98ec1
sagarv1997 <sagarv1997@gmail.com>
2017-10-20 23:13:50 +0530

    A Commit

--- includes/noticeb.inc.php
+++ includes/noticeb.inc.php
@@ -1,8 +1,22 @@
 <?php
 
+	include_once 'dbh.inc.php';
+	include_once 'header.inc.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>View Noiceboard</title>
+</head>
+<body>
+<section class="main-container">
+	<div class="main-wrapper">
+		<div class="noticeboard">
+		<?php
+
 if (isset($_POST['submit'])) {
 
-	include_once 'dbh.inc.php';
 
 	$notice_did = $_POST['notice_did'];
 	$sql = "SELECT * FROM noticeboard WHERE notice_did = $notice_did";
@@ -22,3 +36,6 @@ else {
 	header ("Location: ../noticeb.php?error");
 				exit();
 }
+
+
+
