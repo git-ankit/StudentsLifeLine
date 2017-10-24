cd02ac5abf27c75190ea074b7d2733d02d0fab39
sagarv1997 <sagarv1997@gmail.com>
2017-10-24 23:27:31 +0530

    Show Forum CSS Complete

--- forum/showForums.php
+++ forum/showForums.php
@@ -2,23 +2,36 @@
 	include_once '../includes/dbh.inc.php';
 	include_once '../includes/header.inc.php';
 	include_once '../includes/session_manager.inc.php';
-	
-	$sql = "SELECT * FROM `forum` WHERE `status` = 1";
-	$result = $conn->query($sql);
-	$resultCheck = mysqli_num_rows($result);
-	// check if there are any active forums
-	?>
-	<div class="main-wrapper">
-	<?php  
-	if ($resultCheck == 0) {
-		echo "It seems like there are no more active forums.<Volar Morghulis>";
-	} else {
-		echo "<br/><b><h2>These are the active forums:</b><br/><br/>";
-		while($row = $result->fetch_assoc()) {
-			
-		 	echo "Forum Name: ".$row['name']."<br/>Description: ".$row['description']."<br/>";
-		 	echo "<br/><a href = forum.php?id=".$row['id'].">Take me to this forum</a><br/>";	
-		 } 		
-	}
-	?>
-		
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Show Forums | StudentsLifeLine </title>
+</head>
+<body>
+	<div class="main-container">
+		<div class="box">
+			<div class="box-nav">
+				<h2> Active Forums</h2>	
+			</div>
+				<div class="box-list">
+				<?php
+				$sql = "SELECT * FROM `forum` WHERE `status` = 1";
+				$result = $conn->query($sql);
+				$resultCheck = mysqli_num_rows($result);
+					while($row = $result->fetch_assoc()) {
+						?>
+					 	<a href = <?php echo "forum.php?id=".$row['id'].">" ?><h2><?php echo $row['name']?></h2></a>
+					 	<p><?php echo $row['description']."<br>"?></p><hr>
+					<?php 
+					} ?>
+				</div>
+		</div>
+	</div>
+
+</body>
+</html>
+
+
