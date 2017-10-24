b2e5a1760bec651f44c8dea07b91e6d7d798564f
sagarv1997 <sagarv1997@gmail.com>
2017-10-25 00:08:05 +0530

    Forum CSS Complete

--- forum/forum.php
+++ forum/forum.php
@@ -16,35 +16,62 @@
 		exit();
 	}
 	$row_forum_name = $result_forum_name-> fetch_assoc();
-	echo "<center><h3>".$row_forum_name['name']."</h3></center>";
-	
-	$sql = "SELECT * FROM `thread` WHERE `status` = 1 AND `forum_id` = '$id'";
-	$result = $conn->query($sql);
-	$resultCheck = mysqli_num_rows($result);
-	// check if there are any active forums
-	?>
-	
-	<?php  
-	if ($resultCheck == 0) {
-		echo "<center>It seems like there are no more active threads for this forum.<Volar Morghulis></center>";
-	} else {
-		//echo "<br/><b><h2>These are the active forums:</b><br/><br/>";
-		while($row = $result->fetch_assoc()) {
-			
-		 	echo "Thread Name: ".$row['subject']."<br/>Description: ".$row['description']."<br/>Created On(IST):".$row['created_on']."<br/>";
-		 	if($row['anonymous'] == 'Y') echo "Started By: anonymous";
-		 	else {
-		 		$uid = $row['started_by'];
-		 		$query = "SELECT `user_uid` FROM `users` WHERE `id` = $uid";
-		 		$result_uid = mysqli_query($conn, $query);
-		 		$row_uid = mysqli_fetch_assoc($result_uid);
-		 		$uname = $row_uid['user_uid'];
-		 		echo "Started By: ".$uname;
-		 	}
-		 	echo "<br/><a href = thread.php?id=".$row['id'].">Take me to this thread</a><br/>";
-		 	echo "<br/><br/>";	
-		 } 		
-	}
-	echo "<a href = createthread.php?id=".$id.">Want to create a thread for this forum?</a>";
-	
-	?>
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Forums | StudentsLifeLine</title>
+			<style>
+			table, td, th {    
+		    border: 1px solid #ddd;
+		    text-align: left;
+			}
+
+			table {
+		    border-collapse: collapse;
+		    width: 100%;
+			}
+
+			th, td {
+		    padding: 15px;
+			}
+			</style>
+</head>
+<body>
+	<div class="main-container">
+		<div class="box">
+			<div class="box-nav">
+				<h2> <?php echo $row_forum_name['name'] ?></h2></div>
+					<div class="box-list">
+							<table>
+								<tr>
+									<th>Thread Name</th>
+									<th>Posted By</th>
+									<th>Posted on</th>
+								</tr>
+								<?php
+								$sql = "SELECT * FROM `thread` WHERE `status` = 1 AND `forum_id` = '$id'";
+								$result = $conn->query($sql);
+								$resultCheck = mysqli_num_rows($result);
+									while($row = $result->fetch_assoc()) {
+								?>
+				
+								<tr>
+									<td><a href = <?php echo "thread.php?id=".$row['id'].">" ?><p><?php echo $row['subject']?></p></a></td>
+									<td><?php if($row['anonymous'] == 'Y') echo "anonymous";
+										 	else {
+										 		$uid = $row['started_by'];
+										 		$query = "SELECT `user_uid` FROM `users` WHERE `id` = $uid";
+										 		$result_uid = mysqli_query($conn, $query);
+										 		$row_uid = mysqli_fetch_assoc($result_uid);
+										 		$uname = $row_uid['user_uid'];
+										 		echo $uname; } ?>
+									</td>
+									<td><?php echo $row['created_on'] ?></td> 			
+								</tr>
+								<?php } ?>
+								</table>
+
+</body>
+</html>
