e2d975b46d6dba309d40141d1fadbb462b24f53b
Ankit Tiwari <imankitiwari@gmail.com>
2017-10-24 21:23:21 +0530

    database added

--- studentslifeline.sql
+++ studentslifeline.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 04, 2017 at 06:01 PM
+-- Generation Time: Oct 24, 2017 at 03:52 PM
 -- Server version: 5.7.14
 -- PHP Version: 5.6.25
 
@@ -34,6 +34,14 @@ CREATE TABLE `forum` (
   `created_by` int(11) NOT NULL,
   `status` int(2) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `forum`
+--
+
+INSERT INTO `forum` (`id`, `name`, `description`, `date_created`, `created_by`, `status`) VALUES
+(1, 'First thing', 'This is the very first forum on this page!', '2017-10-05 17:53:11', 2, 1),
+(2, 'Test', 'Test ke liye banaya', '2017-10-05 18:50:03', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -92,11 +100,20 @@ CREATE TABLE `post` (
   `id` int(11) NOT NULL,
   `post` varchar(2000) NOT NULL,
   `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `reply_to` int(11) NOT NULL,
+  `reply_to` int(11) DEFAULT NULL,
   `thread_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `status` int(11) DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `post`
+--
+
+INSERT INTO `post` (`id`, `post`, `date_created`, `reply_to`, `thread_id`, `user_id`, `status`) VALUES
+(1, 'first post', '2017-10-24 14:48:59', NULL, 11, 2, 1),
+(2, 'second post', '2017-10-24 15:29:20', NULL, 11, 2, 1),
+(3, 'third post', '2017-10-24 15:29:41', NULL, 11, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -107,12 +124,25 @@ CREATE TABLE `post` (
 CREATE TABLE `thread` (
   `id` int(11) NOT NULL,
   `subject` varchar(10000) NOT NULL,
+  `description` varchar(6000) NOT NULL,
   `started_by` varchar(100) NOT NULL,
   `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `anonymous` varchar(2) NOT NULL DEFAULT 'N',
   `status` int(10) NOT NULL DEFAULT '1',
   `forum_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `thread`
+--
+
+INSERT INTO `thread` (`id`, `subject`, `description`, `started_by`, `created_on`, `anonymous`, `status`, `forum_id`) VALUES
+(11, 'blah', 'lol', '2', '2017-10-24 14:02:00', 'N', 1, 1),
+(12, 'lol', 'blah', '2', '2017-10-24 14:06:05', 'Y', 1, 1),
+(13, 'opo', 'vovo', '2', '2017-10-24 14:06:48', 'N', 1, 1),
+(14, 'joke', 'chalu hai', '2', '2017-10-24 14:08:50', 'N', 1, 1),
+(15, 'kem', 'palty', '2', '2017-10-24 14:09:20', 'N', 1, 1),
+(16, 'tm', 'loc', '2', '2017-10-24 14:09:48', 'N', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -139,7 +169,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `user_first`, `user_last`, `user_uid`, `user_pwd`, `user_email`, `is_staff`, `date_joined`, `last_login`, `is_superadmin`, `user_status`) VALUES
-(1, 'Ankit', 'Tiwari', 'aat', '$2y$10$fowNF9oQcpBiIMre.BAjTO9sMGXsaSVfoYgVa.5tgrNt8BGhGlOeS', 'aat@somaiya.edu', 'N', '2017-10-04 17:46:40', NULL, 'N', 1);
+(2, 'Ankit', 'Tiwari', 'aat', '$2y$10$JVSe8/epCuq7AUOWqG4kj.wx96g/luGPxA//hvvhExmIUy39BZ47S', 'aat@somaiya.edu', 'N', '2017-10-05 17:51:49', NULL, 'N', 1);
 
 -- --------------------------------------------------------
 
@@ -151,6 +181,18 @@ CREATE TABLE `user_thread` (
   `user_id` int(11) NOT NULL,
   `thread_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_thread`
+--
+
+INSERT INTO `user_thread` (`user_id`, `thread_id`) VALUES
+(2, 11),
+(2, 12),
+(2, 13),
+(2, 14),
+(2, 15),
+(2, 16);
 
 --
 -- Indexes for dumped tables
@@ -212,7 +254,7 @@ ALTER TABLE `user_thread`
 -- AUTO_INCREMENT for table `forum`
 --
 ALTER TABLE `forum`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `moderator_actions`
 --
@@ -227,17 +269,17 @@ ALTER TABLE `noticeboard`
 -- AUTO_INCREMENT for table `post`
 --
 ALTER TABLE `post`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `thread`
 --
 ALTER TABLE `thread`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
