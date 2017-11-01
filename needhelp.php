5681066a3bd921cc7bff44b7aa3d429ad5279463
sagarv1997 <sagarv1997@gmail.com>
2017-11-01 23:55:07 +0530

    needhelp

---
+++ needhelp.php
@@ -0,0 +1,35 @@
+<?php
+	session_start();
+	include_once 'includes/header.inc.php';
+?>
+
+<section class="main-container" style="background-color: #ffa26a">
+	<div class="main-wrapper">
+		<h2>Need Help? You're in the right place</h2></br>
+			<div class="box-about" style="background-color: #56c85c; color: white">
+				<p style="font-size: 18px"> Overwhelmed by stress? You don't have to be. These stress management tips can help you drastically reduce your stress levels and regain control of your life.</p>
+				</br>
+				<p style="font-size: 18px"> If you’re living with high levels of stress, you’re putting your entire well-being at risk. Stress wreaks havoc on your emotional equilibrium, as well as your physical health. It narrows your ability to think clearly, function effectively, and enjoy life.
+
+				Effective stress management, on the other hand, helps you break the hold stress has on your life, so you can be happier, healthier, and more productive. The ultimate goal is a balanced life, with time for work, relationships, relaxation, and fun—and the resilience to hold up under pressure and meet challenges head on. But stress management is not one-size-fits-all. That’s why it’s important to experiment and find out what works best for you. The following stress management tips can help you do that. </p>
+			</div>
+</section>
+
+<section>
+			</br>
+			<h2>Who we are?</h2>
+			</br>
+
+			<div class="box" style="background-color: #697efc">
+				<p style="font-size: 18px; color: white"> We the developers are the also the students and studying Engineering and we understand the problem of each and every student, so being a student we are trying to help each and every student to overcome the difficulty of hesitation or whatever the difficult.</p>
+				</br>
+				<center><img src="/studentslifeline/includes/about.jpg" height="245.4px" width="458px"></center>
+			</div>
+
+
+		
+	</div>
+</section>
+
+</body>
+</html>
